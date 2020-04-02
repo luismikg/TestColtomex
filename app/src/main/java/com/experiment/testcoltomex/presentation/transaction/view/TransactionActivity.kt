cd6c5b778ed02686b7fbc02c46258fbecb519e6f
luismikg <luismikg@gmail.com>
2020-04-02 03:26:33 -0600

    Finish viewmodel-transaction

--- app/src/main/java/com/experiment/testcoltomex/presentation/transaction/view/TransactionActivity.kt
+++ app/src/main/java/com/experiment/testcoltomex/presentation/transaction/view/TransactionActivity.kt
@@ -2,10 +2,18 @@ package com.experiment.testcoltomex.presentation.transaction.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.experiment.testcoltomex.R
+import com.experiment.testcoltomex.databinding.ActivityTransactionBinding
+import com.experiment.testcoltomex.presentation.transaction.viewmodal.TransactionViewModel
+import com.experiment.testcoltomex.presentation.transaction.viewmodal.TransactionViewModelFactory
 import com.shopper.deli.presentation.core.base.BaseActivity
 
 class TransactionActivity : BaseActivity() {
+
+    private var activityTransactionBinding: ActivityTransactionBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -15,8 +23,37 @@ class TransactionActivity : BaseActivity() {
         }
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_transaction)
+        activityTransactionBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_transaction)
+        activityTransactionBinding?.viewmodel = ViewModelProvider(
+            this,
+            TransactionViewModelFactory()
+        ).get(TransactionViewModel::class.java)
+
+        activityTransactionBinding?.viewmodel?.status?.observe(
+            this,
+            Observer {
+                it?.let { statusEvent -> checkStatusEventPopupPrinter(statusEvent) }
+            }
+        )
+
         supportActionBar?.hide()
+    }
+
+    private fun checkStatusEventPopupPrinter(status: Int) {
+
+        when (status) {
+            0 -> closeApp()
+            1 -> successTransacion()
+        }
+    }
+
+    private fun closeApp() {
+        finish()
+    }
+
+    private fun successTransacion() {
+        activityTransactionBinding?.viewmodel?.clearAll()
     }
 
 }
